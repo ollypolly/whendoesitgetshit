915574001e125631d9dd9cdd8f40eee086dd86f9
Olly Pethick <ollypethick@gmail.com>
2024-02-25 23:06:32 +0000

    Update dropoff function

--- src/api/getSeasonRatings.ts
+++ src/api/getSeasonRatings.ts
@@ -5,12 +5,12 @@ type SeasonRatings = Season[];
 // TODO Fix issue where futurama returns 0 as the maxDropoff and undefined in the app
 export const getSeasons = async (seriesId: number) => {
   const showDetails = await getShowDetails(seriesId);
-  const seasonRatings: SeasonRatings = showDetails.seasons.map(
-    (season) => season
-  );
+  let seasonRatings: SeasonRatings =
+    showDetails.seasons.map((season) => season) ?? [];
 
   // Remove first index if the rating is 0
   if (seasonRatings[0].vote_average === 0) {
+    // remove
     seasonRatings.shift();
   }
 
--- src/lib/getDropoff.ts
+++ src/lib/getDropoff.ts
@@ -7,8 +7,10 @@ import { Season } from "@/api/getShowDetails";
 
 export const getDropoff = (seasons: Season[]): Season | undefined => {
   let maxDropoff = 0;
-  let maxDropoffSeason = 0;
+  let maxDropoffSeason = 1;
   let hasFoundDropoff = false;
+  let biggestDropoff = 0;
+  let biggestDropoffSeason = 1;
   for (let i = 0; i < seasons.length - 1; i++) {
     const currentSeason = seasons[i];
     const nextSeason = seasons[i + 1];
@@ -16,11 +18,24 @@ export const getDropoff = (seasons: Season[]): Season | undefined => {
       currentSeason.vote_average - nextSeason.vote_average
     );
 
+    if (dropoff > biggestDropoff) {
+      biggestDropoff = dropoff;
+      biggestDropoffSeason = nextSeason.season_number;
+    }
+
+    // Set biggest dropoff
+
     if (dropoff > maxDropoff && !hasFoundDropoff && dropoff >= 0.5) {
       hasFoundDropoff = true;
       maxDropoff = dropoff;
       maxDropoffSeason = nextSeason.season_number;
     }
+  }
+
+  if (biggestDropoff < 0.5) {
+    return seasons.find(
+      (season) => season.season_number === biggestDropoffSeason
+    );
   }
 
   console.log(maxDropoffSeason, "maxDropoffSeason");
