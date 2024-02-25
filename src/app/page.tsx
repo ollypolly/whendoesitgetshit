2bcf85b588810098b5c9789c4d6dc67dce511b8e
Olly Pethick <ollypethick@gmail.com>
2024-02-25 19:20:45 +0000

    Add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-import Image from "next/image";
+import { Button } from "@/ui/button";
 import styles from "./page.module.css";
+import { getTVShowRating } from "@/api";
 
 export default function Home() {
+  const handleClick = () => {
+    console.log(getTVShowRating("Breaking Bad"));
+  };
+
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>When does it get shit?</h1>
+      <Button onClick={handleClick}>Search</Button>
     </main>
   );
 }
