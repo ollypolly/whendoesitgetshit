84d41b47d78fa6d46a923f370e75e27efac53695
Olly Pethick <ollypethick@gmail.com>
2024-02-27 01:30:45 +0000

    Wrap app in BrowserRouter and QueryParamProvider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,16 +4,23 @@ import { darkTheme } from "@/theme/theme";
 import { ThemeProvider } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { QueryParamProvider } from "use-query-params";
+import { ReactRouter6Adapter } from "use-query-params/adapters/react-router-6";
+import { BrowserRouter } from "react-router-dom";
 
 export const Providers = ({
   children,
 }: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <AppRouterCacheProvider>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        {children}
-      </ThemeProvider>
-    </AppRouterCacheProvider>
+    <BrowserRouter>
+      <QueryParamProvider adapter={ReactRouter6Adapter}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </QueryParamProvider>
+    </BrowserRouter>
   );
 };
